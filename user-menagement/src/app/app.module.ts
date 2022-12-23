996ccee964e00a5695dda8fa6297d258fcf74a1a
catcatproer <1901749687@qq.com>
2022-12-23 09:37:57 +0800

    修改根组件

--- user-menagement/src/app/app.module.ts
+++ user-menagement/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { UsersComponent } from './users/users.component';
+import { FormsModule } from '@angular/forms';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
