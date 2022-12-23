2239e4851b0ff6b52fb3be86f52e792267c7793e
catcatproer <1901749687@qq.com>
2022-12-23 11:10:30 +0800

    添加用户详情组件

--- user-menagement/src/app/user-detail/user-detail.component.ts
+++ user-menagement/src/app/user-detail/user-detail.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../user'
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
+  @Input() user: User | undefined
 
   constructor() { }
 
