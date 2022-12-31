d33b88910fe26399fae0e69e357444a1ba50145a
catcatproer <1901749687@qq.com>
2022-12-31 15:08:57 +0800

    导入用户详情组件和仪表盘组件

--- user-menagement/src/app/app.module.ts
+++ user-menagement/src/app/app.module.ts
@@ -5,15 +5,22 @@ import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { FormsModule } from '@angular/forms';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    MessagesComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
