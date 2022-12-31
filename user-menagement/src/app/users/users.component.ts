f3ac96203e02c53c2d3eba1e6d49818cd7327f30
catcatproer <1901749687@qq.com>
2022-12-31 15:14:10 +0800

    使用Observable方式获取用户数据

--- user-menagement/src/app/users/users.component.ts
+++ user-menagement/src/app/users/users.component.ts
@@ -24,6 +24,7 @@ export class UsersComponent implements OnInit {
   }
 
   getUsers():void{
-    this.users = this.userService.getUsers()
+    // this.users = this.userService.getUsers()
+    this.userService.getUsers().subscribe(users => this.users = users)
   }
 }
